8869c59e6404294040fe9a79f261464d1fcd99b2
santizabe <santizabe@gmail.com>
2023-12-19 21:09:03 +0000

    Add: ft_split

--- ft_split.c
+++ ft_split.c
@@ -12,22 +12,64 @@
 
 #include "libft.h"
 #include <stdlib.h>
+#include <stdio.h>
+
+static void	free_memory(char **str_arr)
+{
+	while (str_arr && *str_arr)
+	{
+		free(*str_arr);
+		str_arr++;
+	}
+	free(str_arr);
+}
+
+static int	word_count(const char *s, char c)
+{
+	int	wc;
+
+	wc = 0;
+	while (s && *s)
+		if (*s++ != c && ++wc)
+			s = ft_strchr(s, c);
+	return (wc);
+}
+
+static int	word_len(const char *s, char c)
+{
+	int	wlen;
+
+	wlen = 0;
+	while (*s && *s++ != c)
+		wlen++;
+	return (wlen);
+}
 
 char	**ft_split(char const *s, char c)
 {
-	int		wc;
-	char	*tmp;
+	int		wlen;
+	int		i;
 	char	**str_arr;
 
-	wc = 0;
-	tmp = (char *)s;
-	while (*tmp && tmp)
-		if (*tmp++ != c && ft_strchr(tmp, c) && ++wc)
-			tmp = ft_strchr(tmp, c);
-	str_arr = (char **)malloc((wc + 1) * sizeof(char *));
+	str_arr = (char **)malloc((word_count(s, c) + 1) * sizeof(char *));
 	if (!str_arr)
 		return (str_arr);
-	str_arr[wc + 1] = NULL;
+	i = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			wlen = word_len(s, c);
+			str_arr[i] = ft_substr(s, 0, wlen);
+			if (!str_arr[i])
+				free_memory(str_arr);
+			i++;
+			s += wlen;
+			continue ;
+		}
+		s++;
+	}
+	str_arr[i] = NULL;
 	return (str_arr);
 }
 
@@ -35,7 +77,6 @@ char	**ft_split(char const *s, char c)
 // {
 // 	char	**arr_list;
 // 	int		i = 0;
-
 // 	arr_list = ft_split("Hola mundo!", ' ');
 // 	while(arr_list[i] != NULL)
 // 	{
